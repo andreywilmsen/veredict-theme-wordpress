eece7514296e20d25d591070a092feefba195b52
andreywilmsen <andreywilmsendepaula@gmail.com>
2026-03-28 20:09:58 -0300

    feat: separa css do header para organização

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@ function veredict_scripts()
     wp_enqueue_style('veredict-grid', get_template_directory_uri() . '/assets/css/grid.css', array(), '1.0.0');
     wp_enqueue_style('veredict-utilities', get_template_directory_uri() . '/assets/css/utilities.css', array(), '1.0.0');
     wp_enqueue_style('veredict-style', get_stylesheet_uri(), array(), '1.0.0');
+    wp_enqueue_style('veredict-header', get_template_directory_uri() . '/assets/css/header.css', array('veredict-style'), '1.0.0');
     wp_enqueue_script('veredict_scripts', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'veredict_scripts');
